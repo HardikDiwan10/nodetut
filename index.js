098e52f46d48e84e4268a9c42720eef51cd36246
HardikDiwan10 <hardikdiwan10@gmail.com>
2022-02-23 16:00:56 +0530

    role and user

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require("express")
-const sessionController = require("./controller/session-controller")
+const mongoose = require("mongoose")
 
+const sessionController = require("./controller/session-controller")
+const roleController = require("./controller/role-controller")
+const userController = require("./controller/user-controller")
 
 const app = express()
+
 //middle ware 
 app.use(express.json()) //mobile -> accept json data from request and set data into body 
 app.use(express.urlencoded({extended:true})) //web --> accept url encoded data from request and set data into body  
 
+//databse
+mongoose.connect('mongodb://localhost:27017/Portfolio',function(err){
+if(err){
+  console.log("db connection fail..");
+  console.log(err);
+  }else{
+    console.log("db connected...");
+  }
+})
 
+
+
+//urls
 app.get("/",function(req,res){
     res.write("welcome...")
     res.end()
@@ -18,6 +34,19 @@ app.get("/login",sessionController.login)
 app.get("/signup",sessionController.signup) 
 app.post("/saveuser",sessionController.saveuser)
 
+//role
+app.post("/roles",roleController.addRole)
+app.get("/roles",roleController.getAllRoles)
+app.delete("/roles/:roleId",roleController.deleteRole)
+app.put("/roles",roleController.updateRole)
+
+//user 
+app.post("/users",userController.addUser)
+app.get("/users",userController.getAllUsers)
+app.delete("/users/:userId",userController.deleteUser)
+
+
+//server
 app.listen(3000,function(){
   console.log("server started on 3000");  
 })
